6d794617e02fda2b6e8a69eeda7665d98994d9e7
Francisco M.C.M. <fcasmen658@gmail.com>
2024-06-22 16:03:28 +0200

    Initial commit

---
+++ src/CaracteresEspeciales02.java
@@ -0,0 +1,11 @@
+/**
+ * Muestra caracteres especiales
+ *
+ * @author Francisco M.C.M.
+ */
+public class CaracteresEspeciales02 {
+    public static void main(String[] args) {
+        System.out.println("Una corchea: \u266A");
+        System.out.print("Dos corcheas: \u266B");
+    }
+}
