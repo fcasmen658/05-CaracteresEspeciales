8fe1ae5ae16f8fec96f568bccc6cbd9996f540bf
fcasmen658 <147444484+fcasmen658@users.noreply.github.com>
2024-06-22 16:06:04 +0200

    Update CaracteresEspeciales02.java

--- src/CaracteresEspeciales02.java
+++ src/CaracteresEspeciales02.java
@@ -5,6 +5,9 @@
  */
 public class CaracteresEspeciales02 {
     public static void main(String[] args) {
+        System.out.print("Cuando programo ⌨ estoy");
+        System.out.print(" muy 😆 y me siento como un ♔.");
+        System.out.println("");
         System.out.println("Una corchea: \u266A");
         System.out.print("Dos corcheas: \u266B");
     }
